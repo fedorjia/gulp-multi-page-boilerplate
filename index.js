95f16bdd877270f2dc200aaae6c8daf3a8722e91
fedor.jia <332762488@qq.com>
2016-10-02 16:14:34 +0800

    change order

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ const conf = require('./webapp/build/conf');
 app.enable('trust proxy');
 
 /** static **/
-const root = settings.debug ? conf.path.src.root : conf.path.dist.root;
 const staticPath = __dirname + '/' + (settings.debug ? conf.path.src.static : conf.path.dist.static);
 app.use('/static', express.static(staticPath, {maxAge: settings.staticMaxAge}));
 
 /** template engine **/
+const root = settings.debug ? conf.path.src.root : conf.path.dist.root;
 nunjucks.configure(root + '/views', {
 	autoescape: true,
 	noCache: settings.debug,
